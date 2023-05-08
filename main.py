01e780ab13cbd6a6e30de674a12e72b0555a911b
Konopelskyi <abrikoskokos42@gmail.com>
2023-05-08 21:29:36 +0300

    display batched images

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datasets import ImagesDataset
-import cv2
 from dataloaders import get_simple_data_loader
 from augmentations import ZoomAndShiftTransform, ReplaceSatelliteImageTransform, MirrorTransform, CustomPILToTensorTransform
+from torchvision import transforms
 
 
 dataset = ImagesDataset("C:\\Users\\Admin\\Desktop\\drone__visual_odometry\\Dataset",
@@ -15,5 +15,14 @@ dataset = ImagesDataset("C:\\Users\\Admin\\Desktop\\drone__visual_odometry\\Data
 data_loader = get_simple_data_loader(dataset)
 
 for i_batch, sample_batched in enumerate(data_loader):
-    print(sample_batched)
+
+    if i_batch == 0:
+
+        to_image_transform = transforms.ToPILImage()
+        drone_image = to_image_transform(sample_batched["drone_image"][0])
+        satellite_image = to_image_transform(sample_batched["satellite_image"][0])
+
+        print(sample_batched["satellite_image_contains_drone_image"][0])
+        drone_image.show()
+        satellite_image.show()
 
